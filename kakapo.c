b52364e6cdd4a8ff66ba1d6e3686ac43621c272c
nic <you@xps13>
2019-03-31 16:35:00 +0100

    remove dead code

--- kakapo.c
+++ kakapo.c
@@ -43,35 +43,6 @@ char * showtype (unsigned char msgtype) {
       default : return "UNKNOWN";
     }
 }
-/*
-unsigned char *toHex (unsigned char *buf, unsigned int l) {
-
-  unsigned char     hex_str[]= "0123456789abcdef";
-  unsigned int      i;
-  unsigned char *result;
-
-  if (!(result = (unsigned char *)malloc(l * 2 + 1)))
-    return (NULL);
-
-  (result)[l * 2] = 0;
-
-  if (!l)
-    return (NULL);
-
-  for (i = 0; i < l; i++)
-    {
-      (result)[i * 2 + 0] = hex_str[(buf[i] >> 4) & 0x0F];
-      (result)[i * 2 + 1] = hex_str[(buf[i]     ) & 0x0F];
-    }
-  return (result);
-}
-
-void printHex ( FILE * fd, unsigned char *buf, unsigned int l) {
-      unsigned char *hex = toHex (buf,l) ;
-      fprintf(fd, "[%s]\n",hex);
-      free(hex);
-}
-*/
 
 int getBGPMessage (struct sockbuf *sb) {
    char *header;
