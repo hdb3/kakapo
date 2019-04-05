ec035206d8fcb4050cd88df82f2087b8c397c624
Nicholas Hart <nic@hdb3.net>
2019-04-05 08:58:06 +0100

    marking the 1.1 tag and creating an update log

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -ve
 gcc -pthread -O3 -DTIMEOUT=1 -D_GNU_SOURCE -o kakapo main.c session.c util.c sockbuf.c
-gcc -pthread -O3 -DSLEEP=5000 -DVERBOSE=1 -DTIMEOUT=1 -D_GNU_SOURCE -o kakapo.loop main.c session.c util.c sockbuf.c
+gcc -pthread -O3 -DSLEEP=5000 -DVERBOSE=0 -DTIMEOUT=1 -D_GNU_SOURCE -o kakapo.loop main.c session.c util.c sockbuf.c
+#gcc -pthread -O3 -DSLEEP=5000 -DVERBOSE=1 -DTIMEOUT=1 -D_GNU_SOURCE -o kakapo.loop main.c session.c util.c sockbuf.c
