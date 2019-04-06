143c7377db593f81afa6f73de46f6fe6868ad2e4
Nicholas Hart <nic@hdb3.net>
2019-04-06 17:52:32 +0100

    made bytestring a library

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ve
 gcc -pthread -O3 -DTIMEOUT=1 -D_GNU_SOURCE -o kakapo main.c session.c util.c sockbuf.c timedloop.c timespec.c
 gcc -pthread -O3 -DMYAS=65007 -DSLEEP=5000 -DVERBOSE=0 -DTIMEOUT=1 -D_GNU_SOURCE -o kakapo.loop main.c session.c util.c sockbuf.c timedloop.c timespec.c
-gcc -pthread -O3 timedloop-unittest.c timedloop.c timespec.c -o timedloop-unittest
+gcc -D_GNU_SOURCE -pthread -O3 timedloop-unittest.c timedloop.c timespec.c -o timedloop-unittest
+gcc -D_GNU_SOURCE -O3 -o bytestring-unittest bytestring-unittest.c bytestring.c util.c
 #gcc -pthread -O3 -DSLEEP=5000 -DVERBOSE=1 -DTIMEOUT=1 -D_GNU_SOURCE -o kakapo.loop main.c session.c util.c sockbuf.c
