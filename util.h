c868b1c5320149b13aa135106477999a04fedc33
nic <you@xps13>
2019-04-06 16:43:15 +0100

    get rid ugly unsigned in util

--- util.h
+++ util.h
@@ -11,8 +11,8 @@ char * hex8(uint8_t n);
 char * hex16(uint16_t n);
 char * hex32(uint32_t n);
 char * hex64(uint64_t n);
-void printHex ( FILE * fd, unsigned char *buf, unsigned int l);
-unsigned char *toHex (unsigned char *buf, unsigned int l);
+void printHex ( FILE * fd, char *buf, int l);
+char *toHex (char *buf, int l);
 
 long long int getinttime ();
 long long int timeval_to_int (struct timeval *tval);
