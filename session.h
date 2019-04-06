7368290f2d624a874db20808992cb08c86c50448
nic <you@xps13>
2019-04-06 10:53:29 +0100

    move get IP address from socket operations into session from main

--- session.h
+++ session.h
@@ -1,16 +1,14 @@
 
 /* kakapo-session - a BGP traffic source and sink */
+// sessiondata is the structure passed to new threads from main
 
 struct sessiondata {
  int sock;
  int tidx;
- int localip;
- int peerip;
  int as;
  char * fn1;
  char * fn2;
 };
 
-//void session(int sock, char * fn1 , char * fn2);
 //*void session(struct sessiondata *sd);
 void *session(void *x);
