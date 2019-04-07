805ee1baccf0df73613cee5fd4a9a3deb3c2831d
Nicholas Hart <nic@hdb3.net>
2019-04-07 19:08:18 +0100

    direct pa list to bytestring

--- update.h
+++ update.h
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include "bytestring.h"
 
+struct bytestring pas2bytestring (char* pa,...);
 struct bytestring pa2bytestring (char* pa);
 //char * paOrigin;
 extern char paOrigin [];
@@ -9,3 +10,4 @@ extern char paLocalPref [];
 extern char paMED [];
 char *paNextHop (uint32_t nexthop);
 char *paASPATH(uint32_t *asn);
+char * rewriteASPATH(char* aspathattribute, uint32_t as, uint8_t index);
