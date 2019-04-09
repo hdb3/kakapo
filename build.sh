669c993dc7612ff133d0d9db21584731e319b803
nic <you@xps13>
2019-04-09 21:53:36 +0100

    sanitising build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -ve
-gcc -pthread -O3 -D_GNU_SOURCE -o kakapo main.c session.c stats.c libutil.c
-#gcc -pthread -O3 -D_GNU_SOURCE -o kakapo main.c session.c util.c sockbuf.c timedloop.c timespec.c
-#gcc -pthread -O3 -DVERBOSE=0 -DTIMEOUT=1 -D_GNU_SOURCE -o kakapo.loop main.c session.c libutil.c
-gcc -D_GNU_SOURCE -pthread -O3 timedloop-unittest.c libutil.c -o timedloop-unittest
-gcc -D_GNU_SOURCE -O3 -o bytestring-unittest bytestring-unittest.c libutil.c
-gcc -D_GNU_SOURCE -O3 libutil.c update-unittest.c -o update-unittest
-#gcc -D_GNU_SOURCE -O3 -o update-unittest update-unittest.c pathattributes.c bytestring.c util.c nlri.c update.c
-#gcc -pthread -O3 -DVERBOSE=1 -DTIMEOUT=1 -D_GNU_SOURCE -o kakapo.loop main.c session.c util.c sockbuf.c
-gcc -D_GNU_SOURCE -I. test/hostaddress.c util.c -o test/hostaddress
+shopt -s expand_aliases
+alias GCC='gcc -pthread -O3 -D_GNU_SOURCE'
+GCC -o kakapo main.c session.c stats.c libutil.c
+GCC timedloop-unittest.c libutil.c -o timedloop-unittest
+GCC -o bytestring-unittest bytestring-unittest.c libutil.c
+GCC libutil.c update-unittest.c -o update-unittest
+GCC -I. test/hostaddress.c util.c -o test/hostaddress
