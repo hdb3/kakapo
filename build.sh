81841fda6e691b4723d7b5b97f8f532c8c40b2fc
Nicholas Hart <nic@hdb3.net>
2019-04-10 11:38:01 +0100

    fix FPE when MAXBURSTCOUNT=0

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 shopt -s expand_aliases
 alias GCC='gcc -pthread -O3 -D_GNU_SOURCE'
 GCC -o kakapo main.c session.c stats.c libutil.c
+GCC -g -o kakapo.debug main.c session.c stats.c libutil.c
 GCC timedloop-unittest.c libutil.c -o timedloop-unittest
 GCC -o bytestring-unittest bytestring-unittest.c libutil.c
 GCC libutil.c update-unittest.c -o update-unittest
