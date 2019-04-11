b1bb9d310db34a7c7167fec7b2a6699256ed2183
nic hart <nic@nic-Precision-Tower-3420>
2019-04-11 13:25:09 +0100

    restore corrupted build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-#!/ bin / bash - ve
-shopt - s expand_aliases alias GCC =
-    'gcc -pthread -O3 -D_GNU_SOURCE' GCC -
-    o kakapo main.c session.c stats.c libutil.c GCC - g -
-    o kakapo.debug main.c session.c stats.c libutil.c GCC timedloop -
-    unittest.c libutil.c - o timedloop - unittest GCC - o bytestring -
-    unittest bytestring - unittest.c libutil.c GCC libutil.c update -
-    unittest.c - o update - unittest GCC - I.test / hostaddress.c util.c -
-    o test / hostaddress
+#!/bin/bash -ve
+shopt -s expand_aliases
+alias GCC='gcc -pthread -O3 -D_GNU_SOURCE'
+GCC -o kakapo main.c session.c stats.c libutil.c
+GCC -g -o kakapo.debug main.c session.c stats.c libutil.c
+GCC timedloop-unittest.c libutil.c -o timedloop-unittest
+GCC -o bytestring-unittest bytestring-unittest.c libutil.c
+GCC libutil.c update-unittest.c -o update-unittest
+GCC -I. test/hostaddress.c util.c -o test/hostaddress
