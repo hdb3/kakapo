60206d34ea981df82ca8e3a27afecfda6c312cda
Nicholas Hart <nic@hdb3.net>
2019-04-14 21:15:43 +0100

    Sysinfo now reports CPUs and cores, too

--- tools/Sysinfo.hs
+++ tools/Sysinfo.hs
@@ -7,15 +7,38 @@ import Data.List (isPrefixOf)
 main = do
     euid <- getEffectiveUserID
     if (euid == 0) then do
-        dmidecode17 <- readProcess "dmidecode" ["--type=17" ] ""
-        let memsize = getMemSize dmidecode17
-        putStrLn $ "memsize is " ++ show memsize ++ "MB"
+        memsize <- getMemSize
+        putStrLn $ "memsize is " ++ show memsize ++ " MB"
+        (cores,cpus) <- getCPUcounts
+        putStrLn $ "core/cpus is " ++ show (cores,cpus)
     else
         putStrLn "Error - root privelege required"
 
-getMemSize :: String -> Int
-getMemSize = sum . map (read . getMemSizeStrings) . filter isSizeDescriptor . lines
+getMemSize :: IO Int
+getMemSize = do
+    dmidecode17 <- readProcess "dmidecode" ["--type=17" ] ""
+    return $ (sum . map (read . getMemSizeStrings) . filter isSizeDescriptor . lines) dmidecode17
     where
     isSizeDescriptor = isPrefixOf "Size:" . dropWhile isSpace
     getMemSizeStrings :: String -> String
     getMemSizeStrings = (!! 1) . words
+
+getCPUcounts :: IO (Int,Int)
+getCPUcounts = do
+    lscpuOutput <- readProcess "lscpu" [] ""
+    let
+        getFirstMatchingKey :: [(String,String)] -> String -> Int
+        getFirstMatchingKey kvx k = ( read . snd . head . filter ( (k ==) . fst )) kvx 
+
+        parselscpuLine :: String -> (String,String)
+        parselscpuLine s = (key,value)
+            where
+            key = takeWhile (':' /=) s
+            value = dropWhile isSpace $ dropWhile (':' ==) $ dropWhile (':' /=) s
+
+        kvs = map parselscpuLine $ lines lscpuOutput
+        threads = getFirstMatchingKey kvs "CPU(s)"
+        sockets = getFirstMatchingKey kvs "Socket(s)"
+        coresPerSocket = getFirstMatchingKey kvs "Core(s) per socket"
+        cores = sockets * coresPerSocket
+    return (cores,threads)
