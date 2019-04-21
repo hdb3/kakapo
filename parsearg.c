1e7894bbee40329e995bfbfab686610243802822
nic hart <nic@hdb3.net>
2019-04-21 22:09:49 +0100

    parsearg working

--- parsearg.c
+++ parsearg.c
@@ -1,50 +1,54 @@
 
-#include <stdio.h>
-#include <stdint.h>
-#include <arpa/inet.h>
-#include "util.h"
 #include "parsearg.h"
+#include "util.h"
+#include <arpa/inet.h>
+#include <stdint.h>
+#include <stdio.h>
 
-args_t commaparse (char *s) {
-  static char *argv [LIMIT];
-  int  argc = 1;
+args_t commaparse(char *s) {
+  static char *argv[LIMIT];
+  int argc = 1;
   argv[0] = s;
-  while ( (0 != *s) && (argc < LIMIT ) ) {
-    if ( ',' == *s ) {
+  while ((0 != *s) && (argc < LIMIT)) {
+    if (',' == *s) {
       *s = 0;
       argv[argc++] = s + 1;
     };
     s++;
   };
-  return (args_t) { argc , argv };
+  return (args_t){argc, argv};
 };
 
-struct peer parseargument (char *s) {
+struct peer parseargument(char *s) {
   struct peer p;
-  args_t args = commaparse (s);
+  args_t args = commaparse(s);
 
-  if (0 == *args.argv[0]) // null string in pos 1 valid, use 0.0.0.0, implies this is a listen only peer definition
+  if (0 == *args.argv[0]) // null string in pos 1 valid, use 0.0.0.0, implies
+                          // this is a listen only peer definition
     p.remote = 0;
   else
     p.remote = toHostAddress(args.argv[0]);
 
-  if ( ( args.argc < 2 ) || 0 == *args.argv[1]) // null string in pos 2 valid, use 0.0.0.0, implies this is a default 0.0.0.0 listener
-      p.local = 0;
-    else
-      p.local = toHostAddress(args.argv[1]);
+  if ((args.argc < 2) ||
+      0 == *args.argv[1]) // null string in pos 2 valid, use 0.0.0.0, implies
+                          // this is a default 0.0.0.0 listener
+    p.local = 0;
+  else
+    p.local = toHostAddress(args.argv[1]);
 
-  if ( ( args.argc < 3 ) || 0 == *args.argv[2]) // null string in pos 3 valid, use 0, implies this is a dynamic ASN
+  if ((args.argc < 3) || 0 == *args.argv[2]) // null string in pos 3 valid, use
+                                             // 0, implies this is a dynamic ASN
     p.as = 0;
   else
-    sscanf(args.argv[2],"%d", &p.as);
+    sscanf(args.argv[2], "%d", &p.as);
 
   return p;
 };
 
-char * displaypeer(struct peer p) {
+char *displaypeer(struct peer p) {
   static char rval[42];
   int ix;
-  ix = snprintf(rval,42,"%s , ",fromHostAddress(p.remote));
-       snprintf(rval+ix,42-ix,"%s , %d",fromHostAddress(p.local),p.as);
+  ix = snprintf(rval, 42, "%s , ", fromHostAddress(p.remote));
+  snprintf(rval + ix, 42 - ix, "%s , %d", fromHostAddress(p.local), p.as);
   return rval;
 };
--- parseargtest.c
+++ parseargtest.c
@@ -1,43 +1,44 @@
 
+#include "parsearg.h"
 #include <stdio.h>
 #include <string.h>
-#include "parsearg.h"
 
 // Unit testing
-// Cases: parse empty, valid and invalid subfields in each position, with 1,2,3 adn more parameters.
+// Cases: parse empty, valid and invalid subfields in each position, with 1,2,3
+// adn more parameters.
 
 // commaparse unittest
-void _unittest (char * s) {
-    char locs[255];
-    strncpy(locs,s,254);
-    locs[254]=0;
+void _unittest(char *s) {
+  char locs[255];
+  strncpy(locs, s, 254);
+  locs[254] = 0;
 
-    printf("commaparse(%s)\n",locs);
-    args_t args = commaparse(locs);
-    printf("returned %d values\n",args.argc);
-    int c;
-    for (c=0; c < args.argc ; c++) {
-      printf ("%d: %s\n",c,args.argv[c]);
-    };
+  printf("commaparse(%s)\n", locs);
+  args_t args = commaparse(locs);
+  printf("returned %d values\n", args.argc);
+  int c;
+  for (c = 0; c < args.argc; c++) {
+    printf("%d: %s\n", c, args.argv[c]);
+  };
 };
 
 // parseargument unittest
-void unittest (char * s) {
-    char locs[255];
-    strncpy(locs,s,254);
-    locs[254]=0;
+void unittest(char *s) {
+  char locs[255];
+  strncpy(locs, s, 254);
+  locs[254] = 0;
 
-    printf ("\'%s\' ~",locs);
-    fflush(stdout);
-    printf ("~ %s\n",displaypeer(parseargument(locs)));
-    ////printf ("%s ~ %s\n",s,displaypeer(parseargument(locs)));
+  printf("\'%s\' ~", locs);
+  fflush(stdout);
+  printf("~ %s\n", displaypeer(parseargument(locs)));
+  ////printf ("%s ~ %s\n",s,displaypeer(parseargument(locs)));
 };
- 
+
 int main(int argc, char **argv) {
-  if ( 1 < argc) {
+  if (1 < argc) {
     int i;
-    for (i=1 ; i < argc ; i++ ) {
-      unittest (argv[i]);
+    for (i = 1; i < argc; i++) {
+      unittest(argv[i]);
     };
   } else {
 
@@ -57,4 +58,3 @@ int main(int argc, char **argv) {
     unittest(",192.168.1.2,12345");
   };
 };
-
