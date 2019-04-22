ff2a8672704b025bdaa25228373fe1defa57dd3f
nic hart <nic@hdb3.net>
2019-04-22 16:11:28 +0100

    current versions of orchestrators

--- tools/Run-kakapo-bird.hs
+++ tools/Run-kakapo-bird.hs
@@ -5,22 +5,23 @@ import System.Exit
 
 main = do
     let
-        -- sut = ssh [ "root@docker02" ]
-        -- kakapo = ssh [ "-p" , "65535" , "root@10.30.65.214" ]
-        -- kakapoHost = ssh [ "root@10.30.65.214" ]
-        sut = ssh [ "root@localhost" ]
-        kakapo = ssh [ "-p" , "65535" , "root@localhost" ]
-        kakapoHost = sut
+        sut = ssh [ "root@docker02" ]
+        kakapo = ssh [ "-p" , "65535" , "root@10.30.65.214" ]
+        kakapoHost = ssh [ "root@10.30.65.214" ]
 
-    sut "docker kill frr"
-    sut "docker pull hdb3/frr ; docker run --name frr --privileged -d -v /var/www/thttpd/:/var/www/thttpd/ --network host --rm hdb3/frr"
+        -- sut = ssh [ "root@localhost" ]
+        -- kakapo = ssh [ "-p" , "65535" , "root@localhost" ]
+        -- kakapoHost = sut
+
+    sut "docker kill bird"
+    sut "docker pull hdb3/bird ; docker run --name bird --privileged -d -v /var/www/thttpd/:/var/www/thttpd/ --network host --rm hdb3/bird"
 
     kakapoHost "docker kill kakapo"
     kakapoHost "docker pull hdb3/kakapo ; docker run --name kakapo --privileged --rm -d --network host --hostname kakapo hdb3/kakapo"
 
     runExperiment kakapo
 
-    sut "docker kill frr"
+    sut "docker kill bird"
     kakapoHost "docker kill kakapo"
 
     putStrLn "Done"
@@ -28,20 +29,19 @@ main = do
 runExperiment :: ([Char] -> IO()) -> IO()
 runExperiment rsh = do
     let
-        logtext = "\"FRR nested batch run 2\""
+        logtext = "\"BIRD nested batch run 1\""
         base = kvSet "LOGTEXT" logtext kakapoDefaultParameters
         gsr = [1..10]
         gsrx = [10,20..50]
         bsr = [1..10] ++ [10,20..100] ++ [100,200..1000] ++ [1000,2000..10000] ++ [10000,20000..100000]
         bsrx = [100000,200000..1000000]
 
-
         genCommands :: String -> [[(String,String)]] -> String -> [String]
         genCommands pre parameterLists post = map (\parameterList -> intercalate " " [pre, expandParameters parameterList, post ]) parameterLists
         expandParameters = intercalate " " . map (\(k,v) -> k ++ "=" ++ v)
         kakapoDefaultParameters =
            [
-             ("LOGTEXT" , "\"auto FRR\" "),
+             ("LOGTEXT" , "\"auto BIRD\" "),
              ("LOGPATH" , "10.30.65.209"),
              ("SLEEP" , "10 "),
              ("MAXBURSTCOUNT" , "1 "),
@@ -63,8 +63,6 @@ runExperiment rsh = do
 
     --die $ buildCommand $ expandParameters base
     -- rsh $ buildCommand $ expandParameters base
-    mapM_ ( rsh . buildCommand ) ( blockGen bsr gsr base )
-
-    --mapM_ runKakapo $ blockGen bsr gsrx base
-    --mapM_ runKakapo $ blockGen bsrx gsr base
-    -- runKakapo $ expandParameters base
+    --mapM_ ( rsh . buildCommand ) ( blockGen bsr gsr base )
+    mapM_ ( rsh . buildCommand ) ( blockGen bsrx gsr base )
+    mapM_ ( rsh . buildCommand ) ( blockGen bsr gsrx base )
