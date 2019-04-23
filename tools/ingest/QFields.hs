e3352c507fedc1bcbcdcd715e190b605413f6b14
nic hart <nic@hdb3.net>
2019-04-23 15:28:47 +0100

    finished work on QFields

--- tools/ingest/QFields.hs
+++ tools/ingest/QFields.hs
@@ -19,57 +19,59 @@ import qualified Data.Text.IO as T
 --   whilst consuming text look for quotation marks, and switch state when detected
 --   add the accumulated string to the list on reaching break condition
 
-isQuote = ( '"' == )
-isSeparator = ( ',' == )
+type QuoteMote = T.Text -> T.Text -> [ T.Text ]
 
 parse :: T.Text -> [T.Text]
 parse = normalMode T.empty 
-
-type QuoteMote = T.Text -> T.Text -> [ T.Text ]
-
--- this assignment hardwirse the behaviour for keeping/dropping quotes
--- a flexible option is easy to build but i wonder how optimal is it when compiled!!!
--- so, for now, there is always some dead code in the module
-quoteMode :: QuoteMote
-quoteMode = removeQuoteMode
-
-normalMode precedingString content =
-    let ( nextChunk, remainder ) = T.break pNormal content
-        acc = precedingString `T.append` nextChunk
-    in if T.null remainder then
-        [ acc ]
-    else if isSeparator ( T.head remainder ) then
-        acc :  normalMode T.empty ( T.tail remainder )
-    else
-        quoteMode acc remainder
     where
-    pNormal c = isQuote c || isSeparator c
 
-keepQuoteMode :: QuoteMote
--- retains the enclosing quote characters
-keepQuoteMode precedingString content =
-    let Just ( first , rest ) = T.uncons content
-        ( nextChunk, remainder ) = T.break isQuote rest
-        acc = precedingString `T.snoc` first `T.append` nextChunk
-    in if T.null remainder then
-        [ acc ]
-    else
-        normalMode ( T.snoc acc (T.head remainder) ) ( T.tail remainder )
-
-removeQuoteMode :: QuoteMote
--- strips the enclosing quote characters
-removeQuoteMode precedingString content =
-    let rest  = T.tail content
-        ( nextChunk, remainder ) = T.break isQuote rest
-        acc = precedingString `T.append` nextChunk
-    in if T.null remainder then
-        [ acc ]
-    else
-        normalMode acc ( T.tail remainder )
-
-test t = T.putStrLn $ "[" `T.append` t `T.append` "] -> " `T.append` ( T.pack $ show $ parse t)
-
+    isQuote = ( '"' == )
+    isSeparator = ( ',' == )
+    
+    
+    -- this assignment hardwirse the behaviour for keeping/dropping quotes
+    -- a flexible option is easy to build but i wonder how optimal is it when compiled!!!
+    -- so, for now, there is always some dead code in the module
+    quoteMode :: QuoteMote
+    quoteMode = removeQuoteMode
+    
+    normalMode precedingString content =
+        let ( nextChunk, remainder ) = T.break pNormal content
+            acc = precedingString `T.append` nextChunk
+        in if T.null remainder then
+            [ acc ]
+        else if isSeparator ( T.head remainder ) then
+            acc :  normalMode T.empty ( T.tail remainder )
+        else
+            quoteMode acc remainder
+        where
+        pNormal c = isQuote c || isSeparator c
+    
+    keepQuoteMode :: QuoteMote
+    -- retains the enclosing quote characters
+    keepQuoteMode precedingString content =
+        let Just ( first , rest ) = T.uncons content
+            ( nextChunk, remainder ) = T.break isQuote rest
+            acc = precedingString `T.snoc` first `T.append` nextChunk
+        in if T.null remainder then
+            [ acc ]
+        else
+            normalMode ( T.snoc acc (T.head remainder) ) ( T.tail remainder )
+    
+    removeQuoteMode :: QuoteMote
+    -- strips the enclosing quote characters
+    removeQuoteMode precedingString content =
+        let rest  = T.tail content
+            ( nextChunk, remainder ) = T.break isQuote rest
+            acc = precedingString `T.append` nextChunk
+        in if T.null remainder then
+            [ acc ]
+        else
+            normalMode acc ( T.tail remainder )
+    
+    
 main = do
+    let test t = T.putStrLn $ "[" `T.append` t `T.append` "] -> " `T.append` ( T.pack $ show $ parse t)
     test ""
     test "123"
     test "123,456"
