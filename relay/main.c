6321db12fd357c2a365fedec777737f594cb18b1
nic hart <nic@hdb3.net>
2019-05-04 15:11:36 +0100

    relay worsk throughh to bind

--- relay/main.c
+++ relay/main.c
@@ -1,21 +1,15 @@
 
-/* kakapo - a BGP traffic source and sink */
+/* kakapo-relay - a BGP traffic relay */
 
 #include <arpa/inet.h>
 #include <assert.h>
 #include <errno.h>
-#include <fcntl.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
-#include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/sendfile.h>
 #include <sys/socket.h>
-#include <sys/time.h>
-#include <time.h>
 #include <unistd.h>
 
 #include <stdint.h>
@@ -26,75 +20,38 @@
 #define SOCKADDRSZ (sizeof(struct sockaddr_in))
 
 struct peer {
-  uint32_t remote;
-  uint32_t local;
-  uint32_t as;
+  // uint32_t remote;
+  // uint32_t local;
+  int sock;
+  struct sockaddr_in remote, local;
 };
 
-struct args {
-  int argc;
-  char **argv;
-};
-typedef struct args args_t;
-args_t commaparse(char *s);
+int getPeer(char *s, struct peer *p) {
 
+  p->remote = (struct sockaddr_in) {AF_INET, htons(179), (struct in_addr){0}};
+  p->local = (struct sockaddr_in) {AF_INET, htons(0), (struct in_addr){0}};
+  0 != parseAddress(s, &(p->local.sin_addr) , &(p->remote.sin_addr) ) ||
+      die("Failed to parse addresses");
 
-// ################################
-
-// number of fields we will collect in a single argument
-#define LIMIT 3 
-
-args_t commaparse(char *s) {
-  static char *argv[3];
-  int argc = 1;
-  argv[0] = s;
-  while ((0 != *s) && (argc < LIMIT)) {
-    if (',' == *s) {
-      *s = 0;
-      argv[argc++] = s + 1;
-    };
-    s++;
-  };
-  return (args_t){argc, argv};
-};
-
-struct peer *parseargument(char *s) {
-  struct peer *p = malloc(sizeof(struct peer));
-  return p;
-};
-
-// ################################
-
-void *clientthread(void *_p) {
-  struct peer *p = (struct peer *)_p;
-  int peersock;
-  struct sockaddr_in peeraddr = {AF_INET, htons(179), (struct in_addr){p->remote}};
-  struct sockaddr_in myaddr = {AF_INET, 0, (struct in_addr){p->local}};
-
-  0 < (peersock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) ||
+  0 < (p->sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) ||
       die("Failed to create socket");
 
-  0 == bind(peersock, &myaddr, SOCKADDRSZ) ||
+  0 == bind(p->sock, &(p->local), SOCKADDRSZ) ||
       die("Failed to bind local address");
 
-  0 == (connect(peersock, &peeraddr, SOCKADDRSZ)) ||
-      die("Failed to connect with peer");
-
 };
 
-void peer(char *s) {
-  struct peer *p = parseargument(s);
-  pthread_t thrd;
-  if (0 == p->remote) // servers have a zero 'remote' address
-    ; // pthread_create(&thrd, NULL, serverthread, (void *)p);
-  else
-    pthread_create(&thrd, NULL, clientthread, (void *)p);
+void run(struct peer* peer1, struct peer* peer2) {
+    printf("run\n");
 };
+
 int main(int argc, char *argv[]) {
 
-  int argn;
-  for (argn = 1; argn <= argc - 1; argn++)
-    peer(argv[argn]);
-  while (1)
-    sleep(100);
+  struct peer peer1, peer2;
+
+  if ( argc != 3 ) die("expecting just two arguments");
+
+  if ( 0 != getPeer(argv[1],&peer1)) die("could not intialise peer for arg 1");
+  if ( 0 != getPeer(argv[2],&peer2)) die("could not intialise peer for arg 2");
+  run(&peer1, &peer2);
 }
