1a5a49a336c7d71b7baa64f781120e5d8b1efa03
nic hart <nic@hdb3.net>
2019-05-07 16:57:18 +0100

    include missing files tcpflags.c tcpflags.h

---
+++ tcpflags.c
@@ -0,0 +1,37 @@
+
+// tcpflags.c
+
+#include "tcpflags.h"
+
+//#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
+
+void flag(int flag, char* flagname, int fd, char* file, int line) {
+    int lopt, opt, rval;
+    lopt = sizeof(opt);
+    rval = getsockopt(fd, IPPROTO_TCP, flag, (void *)&opt, &lopt);
+    assert(rval==0);
+    assert(lopt==sizeof(opt));
+    if (0 == opt) {
+        printf("flag %s found cleared at %s:%d\n", flagname, file, line);
+        opt = 1; lopt = sizeof(opt);
+        rval = setsockopt(fd, IPPROTO_TCP, flag, (void *)&opt, lopt);
+        assert(rval==0);
+        rval = getsockopt(fd, IPPROTO_TCP, flag, (void *)&opt, &lopt);
+        assert(rval==0);
+        assert(lopt==sizeof(opt));
+        assert(opt==1);
+    };
+};
+
+void flags (int fd, char* file, int line) {
+    flag(TCP_NODELAY,"TCP_NODELAY",fd, file, line);
+    flag(TCP_QUICKACK,"TCP_QUICKACK",fd, file, line);
+};
---
+++ tcpflags.h
@@ -0,0 +1,8 @@
+// tcpflags.h
+
+#ifndef __TCPFLAGS_H
+#define __TCPFLAGS_H
+
+void flags (int fd, char* file, int line);
+
+#endif
