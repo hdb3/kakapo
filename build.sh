8573a7aaf9af250ac63099188e496be9b5801da7
nic hart <nic@hdb3.net>
2019-05-15 14:32:57 +0100

    make gdb binaries by default

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -ve
 shopt -s expand_aliases
-alias GCC='gcc -pthread -O3 -D_GNU_SOURCE'
+alias GCC='gcc -g -pthread -O3 -D_GNU_SOURCE'
 GCC -o kakapo main.c session.c stats.c libutil.c parsearg.c
-GCC -g -o kakapo.debug main.c session.c stats.c libutil.c parsearg.c
 GCC timedloop-unittest.c libutil.c -o timedloop-unittest
 GCC -o bytestring-unittest bytestring-unittest.c libutil.c
 GCC libutil.c update-unittest.c -o update-unittest
