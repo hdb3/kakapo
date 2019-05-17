82f1b2817f776515181bb7763e088ac970cd7d66
nic hart <nic@hdb3.net>
2019-05-17 11:08:45 +0100

    WiP

--- ingest/Summarise.hs
+++ ingest/Summarise.hs
@@ -3,7 +3,7 @@ module Main where
 
 import Data.Either(partitionEithers)
 import Data.Maybe(fromMaybe,isJust,fromJust)
-import Data.List (transpose)
+import Data.List (sortOn,transpose,elem)
 import Data.Text(Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -23,4 +23,13 @@ main = do
    mapM_ barf l
    let headers = concatMap fst r
        summary = Summary.summarise headers
-   mapM_ print summary
+   analyse summary
+
+analyse hdrs = do
+   --mapM_ print hdrs
+   let shdrs = sortOn fst $ remove ["START","TIME"] $ hdrs
+   mapM_ print shdrs
+
+   where
+       remove ks = filter ( contains ks . fst )
+       contains set elt = elem elt set
