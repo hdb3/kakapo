5a6eab71c1d87997bfd3039e7cb5232ac11b7095
nic hart <nic@hdb3.net>
2019-05-17 11:22:55 +0100

    WiP

--- ingest/Summarise.hs
+++ ingest/Summarise.hs
@@ -11,6 +11,7 @@ import Data.Attoparsec.Text
 import Control.Applicative((<|>))
 import System.Exit(die)
 import System.Environment(getArgs)
+import Control.Arrow(second)
 import GenParse(getData,Dict,Metrics)
 import Summary
 
@@ -25,11 +26,17 @@ main = do
        summary = Summary.summarise headers
    analyse summary
 
+analyse :: [(Text, [(Text,Int)])] -> IO ()
 analyse hdrs = do
    --mapM_ print hdrs
    let shdrs = sortOn fst $ remove ["START","TIME"] $ hdrs
+       sshdrs = map (second (sortOn snd)) shdrs
    mapM_ print shdrs
+   mapM_ putStrLn ( map display sshdrs)
 
    where
-       remove ks = filter ( contains ks . fst )
+       remove ks = filter ( not . contains ks . fst )
        contains set elt = elem elt set
+       --display (k,vs) = T.unpack k ++ " : " ++ show (length vs) ++ show vs
+       display (k,vs) = T.unpack k ++ " : " ++ show (length vs) ++ " { " ++ unwords ( map show' vs) ++ " }"
+       show' (t,i) = T.unpack t ++ "[" ++ show i ++ "]"
