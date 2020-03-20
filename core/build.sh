660cad6ca352cc2ee67378e0e921e47ef334ea2f
Nicholas Hart <nic@hdb3.net>
2020-03-20 09:06:43 +0000

    gcc wants to have the -lm option at the end of the parameter list it seems...

--- core/build.sh
+++ core/build.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -ve
+#!/bin/bash -xe
 shopt -s expand_aliases
-alias GCC="gcc -g -lm -pthread -O3 -DBUILDDATE=\"\\\"$(date)\\\"\" -DVERSION=\"\\\"$(git describe)\\\"\""
+alias GCC="gcc -g -pthread -O3 -DBUILDDATE=\"\\\"$(date)\\\"\" -DVERSION=\"\\\"$(git describe)\\\"\""
 #GCC -o nlri-unittest nlri-unittest.c libutil.c
 GCC -o logbuffer-test logbuffer-test.c libutil.c
-GCC -o kakapo main.c session.c stats.c libutil.c parsearg.c
+GCC -o kakapo main.c session.c stats.c libutil.c parsearg.c -lm
 #GCC timedloop-unittest.c libutil.c -o timedloop-unittest
 #GCC -o bytestring-unittest bytestring-unittest.c libutil.c
 #GCC libutil.c update-unittest.c -o update-unittest
