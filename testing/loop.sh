a8d228c481912c5e6a047ba189814b779cfa0d20
Nicholas P. Hart <nphart@bisdn.de>
2024-11-27 15:51:21 +0100

    WiP

--- testing/loop.sh
+++ testing/loop.sh
@@ -1,93 +1,100 @@
+#!/bin/bash -e
+SCRIPTDIR=$(realpath $(dirname "$0"))
+: ${BINDIR:="$SCRIPTDIR/bin"}
+: ${BASEDIR:=$(realpath "$SCRIPTDIR/..")}
 
+# test topology
+#
+# DUT receives from multiple internal peers and sends to a single external peer
+#
+# local address: 172.18.0.13
+# external peer address: 172.18.0.19
+# internal peers: 172.18.0.20...
+#
+LOCAL="172.18.0.13"
+EXTERNAL="172.18.0.19"
 #
 ## script control
 
-: ${PLATFORMS:="BIRD BIRD2 FRR OPENBGPD HBGP RELAY GOBGP"}
+# : ${PLATFORMS:="BIRD BIRD2 FRR OPENBGPD HBGP RELAY GOBGP"}
+: ${PLATFORMS:="BIRD2 OPENBGPD FRR RELAY GOBGP"}
 : ${RANGE:="01 05 10 15 20 25 30 35 40 45 50"}
 : ${LOOPCOUNT:=1}
-: ${SRC:="/home/nic/src"}
 
 #
 # some default locations - override in the environment if needed
 # in particular, CONFSUBDIR allows to use other configuration files
 
 : ${CONFSUBDIRS:="simple filters"}
-: ${BINDIR:="$SRC/kagu/build"}
-: ${KAKAPODIR:="$SRC/kakapo/"}
-: ${CONFDIR:="$KAKAPODIR/testing"}
+: ${CONFDIR:="$SCRIPTDIR"}
 #
+
+BIRD_CMD="$BINDIR/bird/bird -d -c "
+BIRD2_CMD="$BINDIR/bird2/bird -d -c "
+OPENBGPD_CMD="$BINDIR/bgpd -d -f "
+GOBGP_CMD="$BINDIR/gobgpd -f "
+FRR_CMD="$BINDIR/frr -S -l $LOCAL -n --log stdout -f  "
+HBGP_CMD="$BINDIR/hbgp "
+RELAY_CMD="$BASEDIR/relay/relay2"
+KAKAPO_CMD="$BASEDIR/bin/kakapo"
 
 ## these are kakapo control variables - must be exported for kakapo process to inherit them
 
-# kakapo variables
+# kakapo command line configuration variables
 : ${REPEAT:=5}
 : ${TIMEOUT:=30}
 : ${TABLESIZE:=160000}
 : ${MAXBURSTCOUNT:=50000}
 : ${GROUPSIZE:=5}
-: ${RATECOUNT:=1000000}
+: ${RATECOUNT:=100000}
 : ${REPEATDELAY:=1}
 : ${WINDOW:=5000}
 : ${MODE:="PAM"}
 KVARS="MAXBURSTCOUNT GROUPSIZE RATECOUNT REPEATDELAY WINDOW TIMEOUT REPEAT MODE TABLESIZE"
 KENV=""
-for v in $KVARS
-  do
-    KENV="${KENV}$v=${!v} "
-  done
+for v in $KVARS; do
+  KENV="${KENV}$v=${!v} "
+done
 
-KAKAPO="$KAKAPODIR/core/kakapo"
+echo "configs in $CONFDIR, binaries in $BINDIR, relay2 in $(dirname $RELAY_CMD)"
+echo "kakapo is $KAKAPO_CMD, platforms: $PLATFORMS"
 
-SCRIPTDIR=$( dirname "${BASH_SOURCE[0]}" )
-VARS=$DIR/vars
+for n in $(seq 1 $LOOPCOUNT); do
+  for CONFSUBDIR in $CONFSUBDIRS; do
+    FULLCONFDIR="${CONFDIR}/$CONFSUBDIR"
 
-#
-
-echo "configs in $CONFDIR, binaries in $BINDIR, relay2 in $( dirname $RELAY )"
-echo "kakapo is $KAKAPO"
-
-for n in `seq 1 $LOOPCOUNT` 
-  do
-     for CONFSUBDIR in $CONFSUBDIRS
-       do
-         FULLCONFDIR="${CONFDIR}/$CONFSUBDIR"
-
-         BIRD="$BINDIR/bird -d -c $FULLCONFDIR/bird.conf"
-         BIRD2="$BINDIR/bird2 -d -c $FULLCONFDIR/bird2.conf"
-         OPENBGPD="$BINDIR/bgpd -d -f $FULLCONFDIR/bgpd.conf"
-         GOBGP="$BINDIR/gobgpd -f $FULLCONFDIR/gobgpd.conf"
-         FRR="$BINDIR/frr -S -l 172.18.0.13 -n --log stdout -f  $FULLCONFDIR/frr.conf"
-         HBGP="$BINDIR/hbgp $FULLCONFDIR/bgp.conf"
-         RELAY="$KAKAPODIR/relay/relay2 172.18.0.13 172.18.0.19"
-    for PLATFORM in $PLATFORMS
-      do
-        BIN=${!PLATFORM}
-        for m in $RANGE
-          do
-          KPEERCONFIG=peers.${m}
-          KPEERS="$(<$SCRIPTDIR/$KPEERCONFIG)"
-          LOGTEXT="$PLATFORM/$CONFSUBDIR/$KPEERCONFIG"
-          KENV+=" LOGTEXT=$LOGTEXT"
-          TESTBIN="$KENV $KAKAPO $KPEERS"
-          # TESTBIN="bash -xe $CONFDIR/${m}peers.sh"
-          echo "run: $n/$m"
-          echo "config: $KPEERCONFIG"
-          echo "platform: $PLATFORM"
-          echo "binary: $BIN"
-          echo "testbin: $TESTBIN"
-          echo "ip netns exec target $BIN & PID=\$!"
-          if [ -z $DRYRUN ]
-          then
-            ip netns exec target $BIN & PID=$!
-            sleep 5 
-            eval "$KENV ip netns exec kakapo $KAKAPO $KPEERS"
-            kill $PID 
-            wait
-          else
-            echo "ip netns exec target $BIN"
-            echo "$KENV ip netns exec kakapo $KAKAPO $KPEERS"
-          fi
-        done
-    done
+    BIRD="$BIRD_CMD $FULLCONFDIR/bird.conf"
+    BIRD2="$BIRD2_CMD $FULLCONFDIR/bird2.conf"
+    OPENBGPD="$OPENBGPD_CMD $FULLCONFDIR/bgpd.conf"
+    GOBGP="$GOBGP_CMD $FULLCONFDIR/gobgpd.conf"
+    FRR="$FRR_CMD $FULLCONFDIR/frr.conf"
+    HBGP="$HBGP_CMD $FULLCONFDIR/bgp.conf"
+    RELAY="$RELAY_CMD $LOCAL $EXTERNAL"
+    for PLATFORM in $PLATFORMS; do
+      BIN=${!PLATFORM}
+      for m in $RANGE; do
+        KPEERCONFIG=peers.${m}
+        KPEERS="$(<$SCRIPTDIR/$KPEERCONFIG)"
+        LOGTEXT="$PLATFORM/$CONFSUBDIR/$KPEERCONFIG"
+        CMD_ENV="$KENV LOGTEXT=$LOGTEXT"
+        echo "run: $n/$m"
+        echo "config: $KPEERCONFIG"
+        echo "platform: $PLATFORM"
+        echo "binary: $BIN"
+        echo "testbin: $TESTBIN"
+        echo "ip netns exec target $BIN & PID=\$!"
+        if [ -z $DRYRUN ]; then
+          ip netns exec target $BIN &
+          PID=$!
+          sleep 5
+          eval "ip netns exec kakapo bash -c \"$CMD_ENV $KAKAPO_CMD $KPEERS\""
+          kill $PID
+          wait
+        else
+          echo "ip netns exec target $BIN"
+          echo "ip netns exec kakapo bash -c \"$CMD_ENV $KAKAPO_CMD $KPEERS\""
+        fi
+      done
     done
   done
+done
--- testing/netns.sh
+++ testing/netns.sh
@@ -1,5 +1,8 @@
+#!/bin/bash -e
 IFT=em3
 IFK=em4
+set -x
+ip link add $IFT type veth peer name $IFK 
 ip netns add target
 ip netns add kakapo
 ip link set $IFT netns target
@@ -8,11 +11,16 @@ ip netns exec target ip li set up dev $IFT
 ip netns exec kakapo ip li set up dev $IFK
 
 ip netns exec target ip addr add 172.18.0.13/24 dev $IFT
+set +x
 for n in `seq 19 70`
   do
     ip netns exec kakapo ip addr add 172.18.0.${n}/24 dev $IFK
     ip netns exec target fping -r 20 -u 172.18.0.${n}
   done
-exit 0
-ip netns del target
-ip netns del kakapo
+  echo "ping test completed between ns:target and ns:kakapo"
+echo "
+# to remove the context, use this:
+sudo ip netns del target &>/dev/null ||:
+sudo ip netns del kakapo &>/dev/null ||:
+sudo ip link delete $IFT &>/dev/null ||:
+"
