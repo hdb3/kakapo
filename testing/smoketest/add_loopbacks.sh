629729f6ed7a167415d666fd9e1909729e10c805
Nicholas P. Hart <nphart@bisdn.de>
2024-12-06 23:29:03 +0100

    support virbr1/VM cases in add_loopbacks.sh

--- testing/smoketest/add_loopbacks.sh
+++ testing/smoketest/add_loopbacks.sh
@@ -1,9 +1,25 @@
 #!/bin/bash -e
+
 if [[ "$1" == "del" ]]; then
     cmd="del"
 else
     cmd="add"
 fi
-for ip in 172.18.0.13 172.18.0.18 172.18.0.19 172.18.0.20; do
-    sudo ip addr $cmd $ip/32 dev lo || echo "error address was $ip"
+
+IFACE=${2:-"lo"}
+
+ADDRESSES="172.18.0.18 172.18.0.19 172.18.0.20"
+
+if [[ "$IFACE" == "lo" ]]; then
+  ADDRESSES="172.18.0.13 $ADDRESSES"
+  MASK="32"
+else
+  MASK="24"
+fi
+
+# TDOD - autodetect what interface owns these addresses, so can remove them without being told
+
+for ip in  $ADDRESSES; do
+    sudo ip addr $cmd $ip/$MASK dev $IFACE || echo "error address was $ip/$MASK"
 done
+
