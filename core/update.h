1405d87023c43039dc6657f072c8390b8d6f3402
Nicholas P. Hart <nphart@bisdn.de>
2024-12-19 00:10:32 +0100

    bug, fixed

--- core/update.h
+++ core/update.h
@@ -1,5 +1,6 @@
 #include "bytestring.h"
 
+char * update_buffered(char *buf, struct bytestring nlri, struct bytestring withdrawn, struct bytestring pathattributes);
 struct bytestring update(struct bytestring nlri, struct bytestring withdrawn, struct bytestring pathattributes);
 struct bytestring iBGPpath(uint32_t nexthop, uint32_t localpref, uint32_t *asn);
 struct bytestring eBGPpath(uint32_t nexthop, uint32_t med, uint32_t *asn);
