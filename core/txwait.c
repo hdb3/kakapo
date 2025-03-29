6cdaf40f3a7d0462081ad3fef97aa0801efa7c78
nicholas hart <nic@hdb3.net>
2025-03-29 09:21:41 +0000

    kakapo/core fixes

--- core/txwait.c
+++ core/txwait.c
@@ -1,10 +1,13 @@
 #include <assert.h>
 #include <linux/sockios.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <time.h>
 
 #include "txwait.h"
+
+extern bool txerror;
 
 // limit wait for cases where the peer disconnected (e.g. when sending to dead peer or fast disconnecting peer)
 #define MAX_TXWAIT_COUNT 200000
@@ -13,6 +16,8 @@ void txwait(int sock) {
   int error, value;
   struct timespec delay = {0, 1000 * 100}; // 100 uS (units are nS)
   for (int i = 0; i < MAX_TXWAIT_COUNT; i++) {
+    if (txerror)
+      break;
     error = ioctl(sock, SIOCOUTQ, &value);
     if (0 == error && value > 0)
       nanosleep(&delay, NULL);
@@ -21,7 +26,10 @@ void txwait(int sock) {
   }
   if (0 != error)
     perror("txwait ioctl problem");
-  else if (value > 0)
-    fprintf(stderr, "txwait timed out wating for transmit queue to empty\n");
+  else if (value > 0) {
+    fprintf(stderr, "txwait timed out waiting for transmit queue to empty\n");
+    close(sock);
+    txerror = true;
+  }
   assert(0 == error);
 };
