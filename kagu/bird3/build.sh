c2a68ca780f9f8ec5447d16a6fe33843e9284694
Nicholas P. Hart <nphart@bisdn.de>
2025-03-30 18:43:47 +0200

    add bird3

---
+++ kagu/bird3/build.sh
@@ -0,0 +1,16 @@
+bird2#!/bin/bash -e
+TARGET_DIR=${1:?"please give a target directory for build output"}
+TARGET_DIR=$(realpath $TARGET_DIR)
+mkdir -p ${TARGET_DIR}
+BUILD_DIR=$(mktemp -d)
+pushd $BUILD_DIR
+
+VERSION=${2:-"3.0.1"}
+
+curl https://bird.network.cz/download/bird-${VERSION}.tar.gz | tar xz
+cd bird-${VERSION}
+./configure --prefix=/
+make
+cp birdc bird ${TARGET_DIR}
+popd
+rm -rf ${BUILD_DIR}
--- kagu/image_build.sh
+++ kagu/image_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-export DOCKER_BUILDKIT=1 DOCKER_HOST=127.0.0.1
+# export DOCKER_BUILDKIT=1 DOCKER_HOST=127.0.0.1
 
-images="ghcup hbgp gobgp bgpd bird bird2 frr"
+images="ghcup hbgp gobgp bgpd bird1 bird2 bird3 frr"
 tagged_build(){
   MD5SUM=$(md5sum $1/Dockerfile | cut -d " " -f1)
   docker build --label dockerfile_md5sum=$MD5SUM -t $1 $1
