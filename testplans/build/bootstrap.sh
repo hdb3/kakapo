078c4a5a8cadf032413b187783f4964c64421b71
nic hart <nphart@otolonetworks.com>
2025-04-14 13:43:32 +0100

    humanise testplans/build/bootstrap.sh

--- testplans/build/bootstrap.sh
+++ testplans/build/bootstrap.sh
@@ -9,19 +9,44 @@
 #
 # the size and checksum are specific to the versions of junos and vious available at the time of writing
 #
-# ./ubuntu-dependencies.sh
-virsh capabilities | grep qemu
-IMAGEDIR=$HOME/vmimages
-mkdir -p $IMAGEDIR
-vios=$IMAGEDIR/vios.vmdk
-junos=$IMAGEDIR/junos.qcow2
-find ~ -name vios-adventerprisek9-m.vmdk.SPA.157-3.M3 -size 134021120c -exec ln -s "{}" $vios \;
-echo "37c148ffa14a82f418a6e9c2b049fafe $vios" | md5sum -c
-#
-find ~ -name junos-vmx-x86-64-18.2R1.9.qcow2 -size 1334771712c -exec ln -s "{}" $junos \;
-echo "491d74204f4971dc6c8f525b6151fc2e $junos" | md5sum -c
-echo "successfully located ios and junos"
 
-# allow kvm/qemu/libvirt to access images in user directories
-chmod a+xr $HOME
-chmod a+xr $IMAGEDIR
+if ! virsh capabilities | grep -q qemu
+then
+  echo "libvirt cannot use qemu"
+else
+  echo "libvirt can use qemu"
+  IMAGEDIR=$HOME/vmimages
+  mkdir -p $IMAGEDIR
+  vios=$IMAGEDIR/vios.vmdk
+  junos=$IMAGEDIR/junos.qcow2
+
+  echo "scanning for cisco VIOS image"
+  if ! find ~ -name vios-adventerprisek9-m.vmdk.SPA.157-3.M3 -size 134021120c -exec ln -s "{}" $vios \;
+  then
+    echo "cisco VIOS image not found"
+  elif ! echo "37c148ffa14a82f418a6e9c2b049fafe $vios" | md5sum -c
+  then
+    echo "cisco VIOS image failed checksum check"
+  else
+    echo "success - cisco VIOS image found"
+  fi
+
+  echo "scanning for juniper vMX image"
+
+  if !   find ~ -name junos-vmx-x86-64-18.2R1.9.qcow2 -size 1334771712c -exec ln -s "{}" $junos \;
+  then
+    echo "juniper vMX image not found"
+  elif !   echo "491d74204f4971dc6c8f525b6151fc2e $junos" | md5sum -c
+  then
+    echo "juniper vMX image failed checksum check"
+  else
+    echo "success - juniper vMX image found"
+  fi
+
+  #
+  echo "successfully located ios and junos"
+
+  # allow kvm/qemu/libvirt to access images in user directories
+  chmod a+xr $HOME
+  chmod a+xr $IMAGEDIR
+fi
