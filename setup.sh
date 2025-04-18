0712c7b9d3f61e5def155441edba34eb1975f708
Nicholas P. Hart <nphart@bisdn.de>
2025-04-18 18:03:56 +0000

    WiP

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ for group in libvirt docker ; do
 	sudo usermod -a -G $group $USER
 done
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r 172.16.102.32:vrouter_images .
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+git clone git@github.com:hdb3/kakapo.git -b dev
 sudo apt install -y make
 newgrp docker
 newgrp libvirt
-
+sudo reboot
 
