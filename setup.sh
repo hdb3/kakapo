128c5cae5942ee1e20b32064b9ff4d247d81fd3a
Nicholas P. Hart <nphart@bisdn.de>
2025-04-18 17:51:15 +0000

    WiP

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
+sudo apt update
+sudo apt -y full-upgrade
 for group in libvirt docker ; do
 	sudo groupadd $group
-	sudo usermod -a -G $group $USERNAME
+	sudo usermod -a -G $group $USER
 done
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r 172.16.102.32:vrouter_images .
 sudo apt install -y make
+newgrp docker
+newgrp libvirt
+
 
