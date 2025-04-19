9b9f021377c39bd04a5a4556f72d56d5e3da6382
Nicholas P. Hart <nphart@bisdn.de>
2025-04-19 17:04:20 +0200

    WiP

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt -y full-upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt -yy full-upgrade
 for group in libvirt docker ; do
 	sudo groupadd $group
 	sudo usermod -a -G $group $USER
@@ -7,5 +7,4 @@ done
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r 172.16.102.32:vrouter_images .
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone git@github.com:hdb3/kakapo.git -b dev
-sudo apt install -y make
-# sudo systemctl reboot
+sudo DEBIAN_FRONTEND=noninteractive apt install -yy make
