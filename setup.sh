18ce8ed75a14a0283f79c7b1fddd961d170fa49d
Nicholas P. Hart <nphart@bisdn.de>
2025-04-19 15:02:00 +0200

    WiP

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r 172.16.102.32
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone git@github.com:hdb3/kakapo.git -b dev
 sudo apt install -y make
-sudo systemctl reboot
+# sudo systemctl reboot
