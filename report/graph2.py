5c10f97426301472f6a3ab6ccdb262a9e4e81afc
nicholas hart <nic@hdb3.net>
2025-06-15 10:34:47 +0200

    WiP

--- report/graph2.py
+++ report/graph2.py
@@ -1,11 +1,26 @@
 import os
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-from linestyle import linestyle_tuple
 import matplotlib.colors as mcolors
 from matplotlib.lines import Line2D
 from matplotlib.legend import Legend
-import inspect
+
+linestyle_tuple = [
+    ("solid", "solid"),
+    ("densely dashdotted", (0, (3, 1, 1, 1))),
+    ("dashed", (0, (5, 5))),
+    ("densely dotted", (0, (1, 1))),
+    # ("loosely dotted", (0, (1, 10))),
+    ("dotted", (0, (1, 5))),
+    ("long dash with offset", (5, (10, 3))),
+    ("loosely dashed", (0, (5, 10))),
+    ("densely dashed", (0, (5, 1))),
+    ("loosely dashdotted", (0, (3, 10, 1, 10))),
+    ("dashdotted", (0, (3, 5, 1, 5))),
+    ("dashdotdotted", (0, (3, 5, 1, 5, 1, 5))),
+    ("loosely dashdotdotted", (0, (3, 10, 1, 10, 1, 10))),
+    ("densely dashdotdotted", (0, (3, 1, 1, 1, 1, 1))),
+]
 
 
 def plot_groups(gxx, plot_text):
